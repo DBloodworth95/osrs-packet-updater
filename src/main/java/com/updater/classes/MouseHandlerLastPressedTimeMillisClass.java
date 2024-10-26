5ba2178196b8d3ef94d98ea6022bcc40f6e31407
DBloodworth95 <dan.bloodworth95@gmail.com>
2024-10-26 12:28:33 +0100

    refactor: migrate MouseHandlerLastPressedTimeMillisClass to new query API

--- src/main/java/com/updater/classes/MouseHandlerLastPressedTimeMillisClass.java
+++ src/main/java/com/updater/classes/MouseHandlerLastPressedTimeMillisClass.java
@@ -2,43 +2,30 @@ package com.updater.classes;
 
 import com.updater.PatternSearcher;
 import com.updater.SearchContext;
+import com.updater.search.SearchQuery;
+import com.updater.search.SearchResult;
 
 import java.io.File;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /*
  * Steps to find are as follows:
  * 1. Look for a class which implements MouseListener, MouseMotionListener & FocusListener.
  */
 public class MouseHandlerLastPressedTimeMillisClass implements PatternSearcher {
-
-    private static final String CLASS_PATTERN = "class\\s+\\w+\\s+implements\\s+MouseListener,\\s*MouseMotionListener,\\s*FocusListener";
-    private final Pattern classPattern;
-
     private String obfuscatedClassName = "Unknown";
-
-    public MouseHandlerLastPressedTimeMillisClass() {
-        this.classPattern = Pattern.compile(CLASS_PATTERN);
-    }
 
     @Override
     public boolean matches(File file, String content, SearchContext context) {
-        Matcher matcher = classPattern.matcher(content);
-        if (matcher.find()) {
-            obfuscatedClassName = extractClassName(content);
-            return true;
-        }
-        return false;
-    }
+        SearchQuery query = new SearchQuery(content)
+                .implementsInterfaces("MouseListener", "MouseMotionListener", "FocusListener")
+                .fromClass();
 
-    private String extractClassName(String content) {
-        Pattern classPattern = Pattern.compile("class\\s+(\\w+)");
-        Matcher matcher = classPattern.matcher(content);
-        if (matcher.find()) {
-            return matcher.group(1);
+        SearchResult result = query.execute();
+        if (result.meetsRequirements()) {
+            obfuscatedClassName = result.getResult();
         }
-        return "Unknown";
+
+        return !obfuscatedClassName.equals("Unknown");
     }
 
     @Override
